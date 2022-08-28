44530425c6c2ed5f0e8ef52c261c54de7515cee6
Sarman <sarman@inbox.ru>
2022-08-28 14:28:43 +0600

    Это первый коммит

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14; 
+// console.log(4/0);
+// console.log(`str`*9);
+// let persone = {
+//     name: 'John',
+//     age: 34,
+//     isMarried: false 
+// };
+// console.log(persone['age']);
+
+// let arr = ['plum.jpg', 'orange.png', 'apple.bmp'];
+// console.log(arr[0]);
+// // alert(`Hello mr. Sarman`);
+// // let answer = confirm(`Tebe est' 18?`);
+// // console.log(answer);
+// let answer = prompt(`Tebe est' 18?`, `Da`);
+// console.log(answer);
+let incr = 10, 
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+console.log(`2`==2);
